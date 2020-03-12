fb88426ed967ae3cfc21e173b46963b4ec4d83ef
nhathuy007 <55850287+nhathuy007@users.noreply.github.com>
2020-03-11 19:51:04 -0700

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const db = {}
 
 
 // Connection to LocalDB
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+/* const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql',
   define: {
@@ -18,20 +18,21 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     idle: 10000
   }
 })
+*/
 
 // Connection to AWS
-// const sequelize = new Sequelize('pmmc', 'admin', '12345678', {
-//   host: 'pmmc1.cwdubawie51w.us-west-1.rds.amazonaws.com',
-//   port: 3306,
-//   logging: console.log,
-//   maxConcurrentQueries: 100,
-//   dialect: 'mysql',
-//   dialectOptions: {
-//     connectTimeout: 60000
-//   },
-//   pool: { maxConnections: 5, maxIdleTime: 30},
-//   language: 'en'
-// })
+ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+   host: process.env.DB_HOST,
+   port: 3306,
+   logging: console.log,
+   maxConcurrentQueries: 100,
+   dialect: 'mysql',
+   dialectOptions: {
+     connectTimeout: 60000
+   },
+   pool: { maxConnections: 5, maxIdleTime: 30},
+   language: 'en'
+ })
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
